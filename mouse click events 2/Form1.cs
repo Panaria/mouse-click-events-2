49c9328007a32970afd3edc2b1e8a3dbac4619d6
Panaria <baranyldzby@gmail.com>
2023-10-18 22:07:58 +0300

    Add project files.

---
+++ mouse click events 2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouse_click_events_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem.ToString());
+            listBox1.Items.Remove(listBox1.SelectedItem.ToString());
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem.ToString());
+            listBox2.Items.Remove(listBox2.SelectedItem.ToString());
+        }
+    }
+}
